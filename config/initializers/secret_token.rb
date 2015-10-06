82a83c7e2b7a1d6c49ba381e9858943892d51bb6
rickjcr3 <rickrieta@gmail.com>
2015-10-05 23:13:31 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rickbox::Application.config.secret_key_base = 'fd8bc8b2dfb4e9310a63be2f074af7a47fa85288f02cd916f98e00cd2467c10f76f1aa5ddacc36dad583c7e9938d7fe78f980e7e6701cfbbf5285d375e0024fb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rickbox::Application.config.session_store :cookie_store, key: '_rickbox_session'
